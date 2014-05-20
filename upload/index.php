727ad17a7f7b493ae3141b7c5624c06f8b292557
Destin <destin.lee@shoutlet.com>
2014-05-20 16:00:19 -0500

    adding some new styles

--- upload/index.php
+++ upload/index.php
@@ -1,46 +1,91 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
 <head>
-    <title>des.li file upload</title>
-    <link rel="stylesheet" type="text/css" href="global.css">
+
+    <!-- Basic Page Needs
+  ================================================== -->
+    <meta charset="utf-8">
+    <title>Image upload by des.li</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Mobile Specific Metas
+  ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- CSS
+  ================================================== -->
+    <link rel="stylesheet" href="../stylesheets/base.css">
+    <link rel="stylesheet" href="../stylesheets/skeleton.css">
+    <link rel="stylesheet" href="../stylesheets/layout.css">
+
+    <!-- js
+    ================================================= -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    
+    <script type="text/javascript">
+
+    </script>
+
+    <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+
 </head>
 <body>
-    <form action="upload_file.php" method="post" enctype="multipart/form-data" class="dark-matter">
-        <h1>Local Upload
-        <span>uploads from your local machine.</span></h1>
-         <label>
-            <span>New Name: </span>
-            <input type="text" name="text" id="text"><br />
-        </label>
-        <label>
-            <span>File Name: </span>
-            <input type="file" name="file" id="file">
-        </label>
-        <label>
-            <span>&nbsp;</span>
-            <input type="submit" name="submit" value="upload" class="button">
-        </label>
-    </form>
+    <div class="header full-width">
+        <div class="container">
+            <div class="sixteen columns">
+            <div class="title">
+                des.li
+            </div>
+            </div>
+        </div>
+    </div>
 
-    <form action="cross_upload_file.php" method="post" enctype="multipart/form-data" class="dark-matter">
+    <div class="upload full-width">
+        <div class="container">
+            <div class="sixteen columns">
+                <form action="upload_file.php" method="post" enctype="multipart/form-data" class="dark-matter">
+                    <label>
+                        <input type="text" name="text" id="text" placeholder="new file name..."><br />
+                    </label>
+                    <label>
+                        <input type="file" name="file" id="file" class="file">
+                    </label>
+                    <label>
+                        <input type="submit" name="submit" value="upload" class="button">
+                    </label>
+                </form>
+            </div>
+        </div>
+    </div>
 
-        <h1>Upload from URL
-        <span>submit the URL of any image and it will be uploaded under the new name</span></h1>
-        <label>
-            <span>New Name: </span>
-            <input type="text" name="text" id="text"><br />
-        </label>
-        <label>
-            <span>Your URL: </span>
-            <input type="text" name="url" /><br />
-        </label>
-        <label>
-            <span>&nbsp;</span>
-            <input type="submit" name="submit" value="crossload" class="button">
-        </label>
+    <div class="crossload full-width">
+        <div class="container">
+            <div class="sixteen columns">
+                <form action="cross_upload_file.php" method="post" enctype="multipart/form-data" class="dark-matter">
+                    <label>
+                        <input type="text" name="text" id="text" placeholder="new file name..."><br />
+                    </label>
+                    <label>
+                        <input type="text" name="url" placeholder="url of image to cross load"><br />
+                    </label>
+                    <label>
+                        <input type="submit" name="submit" value="crossload" class="button">
+                    </label>
 
-    </form>
-
+                </form>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
